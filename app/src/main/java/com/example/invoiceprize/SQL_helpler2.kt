26984c268a3688597806077d69a02c7446d88685
wanCwC2 <wanpcwc0630@gmail.com>
2021-12-30 10:53:24 +0800

    database setting

--- app/src/main/java/com/example/invoiceprize/SQL_helpler2.kt
+++ app/src/main/java/com/example/invoiceprize/SQL_helpler2.kt
@@ -4,18 +4,18 @@ import android.content.Context
 import android.database.sqlite.SQLiteDatabase
 import android.database.sqlite.SQLiteOpenHelper
 
-class SQL_helpler(
+class SQL_helpler2(
     context: Context,
     name: String = database,
     factory: SQLiteDatabase.CursorFactory? = null,
     version: Int = ver
 ): SQLiteOpenHelper(context, name, factory, version) {
         companion object{
-            private const val database = "prize"
+            private const val database = "passbook"
             private const val ver = 1
         }
     override fun onCreate(db: SQLiteDatabase){
-        db.execSQL("CREATE TABLE prize(prize_id PRIMARY KEY, date not null)")
+        db.execSQL("CREATE TABLE passbook(invoice_id PRIMARY KEY, money not null, kind not null, award not null)")
     }
     override fun onUpgrade(db: SQLiteDatabase, oldVersion: Int, newVersion: Int){
         db.execSQL("DROP TABLE IF EXISTS addressBook")
