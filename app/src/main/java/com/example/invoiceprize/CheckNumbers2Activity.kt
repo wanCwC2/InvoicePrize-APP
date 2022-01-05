f43b251bb0b33472ee3acf20661a49eb769bfe98
CWC2 <408570344@gapp.fju.edu.tw>
2022-01-05 17:30:13 +0800

    Reward two period

--- app/src/main/java/com/example/invoiceprize/CheckNumbers2Activity.kt
+++ app/src/main/java/com/example/invoiceprize/CheckNumbers2Activity.kt
@@ -7,13 +7,13 @@ import android.os.Bundle
 import android.text.Editable
 import android.text.TextWatcher
 import android.widget.EditText
+import android.widget.TextView
 import android.widget.Toast
 import com.example.invoiceprize.prize.*
 
 class CheckNumbers2Activity : AppCompatActivity() {
     private lateinit var db: SQLiteDatabase
     private lateinit var rdb: SQLiteDatabase
-    var bool = 0
     val data: Array<Prize> = arrayOf<Prize>(
         SpecialPrize(),
         GrandPrize(),
@@ -26,24 +26,37 @@ class CheckNumbers2Activity : AppCompatActivity() {
         AddsixPrize(),
         NotPrize()
     )
+    var time: String = "0"
+    var index: Int = 0
+    var ed_number: String = "0"
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.check_numbers2)
+        intent?.extras?.let {
+            time = "${it.getString("date")}"
+            index = it.getInt("index")
+            ed_number = "${it.getString("user")}"
+        }
+        if (index > 5){
+            index = index - 6
+        }
 
         db = SQL_helpler(this).writableDatabase
         rdb = SQL_helpler2(this).writableDatabase
 
         //綁定元件
         val ed_invoiceNumber = findViewById<EditText>(R.id.ed_invoiceNumber)
+        val tv_init = findViewById<TextView>(R.id.tv_init)
+        val tv_test2 = findViewById<TextView>(R.id.tv_test2)
+
+        tv_init.text = ed_number
+        tv_init.textSize = 35F
 
         //中獎號碼輸出
-        val time = Time()
-        time.run()
         lateinit var query: String
-        query = "SELECT * FROM prize WHERE date like '${time.timeNow}'"
+        query = "SELECT * FROM prize WHERE date like '${time}'"
         val c = db.rawQuery(query, null)
-        c.moveToFirst()
 
         //監聽輸入文字
         ed_invoiceNumber.addTextChangedListener(object : TextWatcher {
@@ -57,20 +70,20 @@ class CheckNumbers2Activity : AppCompatActivity() {
                 s: CharSequence, start: Int, before: Int,
                 count: Int
             ) {
-                if (ed_invoiceNumber.length() == 8) {
-                    for (i in 0 until 6){
-                        compare(i, ed_invoiceNumber.text.toString().toInt(), c.getString(0).toInt())
-                        c.moveToNext()
-                    }
-                    if (bool == 0) {
+                if (ed_invoiceNumber.length() == 5) {
+                    c.moveToPosition(index)
+                    val user = ed_invoiceNumber.text.toString()+ed_number
+                    val bool = compare(user.toInt(), c.getString(0).toInt())
+                    if (!bool) {
                         rdb.execSQL(
                             "INSERT INTO passbook(invoice_id, date, money, award)" +
-                                    "VALUES('${ed_invoiceNumber.text}','11009','${data[9].bonus}','${data[9].name}')"
+                                    "VALUES('${user}','${time}','${data[9].bonus}','${data[9].name}')"
                         )
-                        showToast("${ed_invoiceNumber.text}未中獎")
+                        showToast("${user}未中獎")
                         ed_invoiceNumber.text.clear()
                         val intent = Intent(applicationContext, CheckNumbersActivity::class.java)
                         startActivity(intent)
+                        finish()
                     }
                 }
             }
@@ -81,7 +94,7 @@ class CheckNumbers2Activity : AppCompatActivity() {
     }
 
     //比對中獎
-    fun compare (index: Int, enter: Int, prize: Int){
+    fun compare (enter: Int, prize: Int): Boolean {
         rdb = SQL_helpler2(this).writableDatabase
         val ed_invoiceNumber = findViewById<EditText>(R.id.ed_invoiceNumber)
 
@@ -89,23 +102,27 @@ class CheckNumbers2Activity : AppCompatActivity() {
             if (enter == prize) {
                 rdb.execSQL(
                     "INSERT INTO passbook(invoice_id, date, money, award)" +
-                            "VALUES('${enter}','11009','${data[0].bonus}','${data[0].name}')"
+                            "VALUES('${enter}','${time}','${data[0].bonus}','${data[0].name}')"
                 )
                 showToast("${enter}中${data[0].name}")
                 ed_invoiceNumber.text.clear()
                 val intent = Intent(applicationContext, CheckNumbersActivity::class.java)
                 startActivity(intent)
+                finish()
+                return true
             }
         } else if (index == 1) {
             if (enter == prize) {
                 rdb.execSQL(
                     "INSERT INTO passbook(invoice_id, date, money, award)" +
-                            "VALUES('${enter}','11009','${data[1].bonus}','${data[1].name}')"
+                            "VALUES('${enter}','${time}','${data[1].bonus}','${data[1].name}')"
                 )
                 showToast("${enter}中${data[0].name}")
                 ed_invoiceNumber.text.clear()
                 val intent = Intent(applicationContext, CheckNumbersActivity::class.java)
                 startActivity(intent)
+                finish()
+                return true
             }
         } else if (index == 2 || index == 3 || index == 4) {
             var number = 2
@@ -114,25 +131,31 @@ class CheckNumbers2Activity : AppCompatActivity() {
             if (number != 9) {
                 rdb.execSQL(
                     "INSERT INTO passbook(invoice_id, date, money, award)" +
-                            "VALUES('${enter}','11009','${data[number].bonus}','${data[number].name}')"
+                            "VALUES('${enter}','${time}','${data[number].bonus}','${data[number].name}')"
                 )
-                showToast("${enter}中${data[0].name}")
+                showToast("${enter}中${data[number].name}")
                 ed_invoiceNumber.text.clear()
                 val intent = Intent(applicationContext, CheckNumbersActivity::class.java)
                 startActivity(intent)
+                finish()
+                return true
             }
         } else if (index == 5 || index == 6) {
             if (enter % 1000 === prize) {
                 rdb.execSQL(
                     "INSERT INTO passbook(invoice_id, date, money, award)" +
-                            "VALUES('${enter}','11009','${data[8].bonus}','${data[8].name}')"
+                            "VALUES('${enter}','${time}','${data[8].bonus}','${data[8].name}')"
                 )
-                showToast("${enter}中${data[0].name}")
+                showToast("${enter}中${data[8].name}")
                 ed_invoiceNumber.text.clear()
                 val intent = Intent(applicationContext, CheckNumbersActivity::class.java)
                 startActivity(intent)
+                finish()
+                return true
             }
         }
+        return false
+
     }
 
     //針對頭獎不同末幾號而判斷
